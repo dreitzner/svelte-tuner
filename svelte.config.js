4413e212b5d7ac08f13ea170731eb9bfc9269392
Domenik Reitzner <domenik@reitzner.at>
2026-04-09 20:41:08 +0200

    Fix: use vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      fallback: 'index.html'
-    })
+    adapter: adapter(),
+
   }
 };
 
