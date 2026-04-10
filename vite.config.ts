0a96d6702926874ea4e5ff129c596d9721363720
Domenik Reitzner <domenik@reitzner.at>
2026-04-10 09:51:06 +0200

    Chore: update to latest packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(),  devtoolsJson()],
   resolve: {
     alias: {
       '$workers': './src/workers'
